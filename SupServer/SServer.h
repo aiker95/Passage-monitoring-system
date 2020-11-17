f08a8b86e4efdbc339962da218b395f901e616e2
Ермилов Дмитрий Юрьевич <dy.ermilov@hq.severstalauto.com>
2020-11-17 14:40:16 +0300

    Add Project template

---
+++ SupServer/SServer.h
@@ -0,0 +1,72 @@
+#ifndef SSERVER_H
+#define SSERVER_H
+#include "includes.h"
+
+//Буффер для приёма данных от клиента
+static constexpr uint16_t buffer_size = 4096;
+
+struct TcpServer {
+    class Client;
+    //Тип Callback-функции обработчика клиента
+    typedef std::function<void(Client)> handler_function_t;
+    //Статус сервера
+    enum class status : uint8_t {
+        up = 0,
+        err_socket_init = 1,
+        err_socket_bind = 2,
+        err_socket_listening = 3,
+        close = 4
+    };
+
+private:
+    uint16_t port; //Порт
+    status _status = status::close;
+    handler_function_t handler;
+
+    std::thread handler_thread;
+    std::list<std::thread> client_handler_threads;
+    std::list<std::thread::id> client_handling_end;
+
+    SOCKET serv_socket = INVALID_SOCKET;
+    WSAData w_data;
+
+    void handlingLoop();
+
+public:
+    TcpServer(const uint16_t port, handler_function_t handler);
+    ~TcpServer();
+
+    //! Set client handler
+    void setHandler(handler_function_t handler);
+
+    uint16_t getPort() const;
+    uint16_t setPort(const uint16_t port);
+
+    status getStatus() const { return _status; }
+
+    status restart();
+    status start();
+    void stop();
+
+    void joinLoop();
+};
+
+class TcpServer::Client {
+    SOCKET socket;
+    SOCKADDR_IN address;
+    char buffer[buffer_size];
+public:
+    Client(SOCKET socket, SOCKADDR_IN address);
+public:
+    Client(const Client& other);
+    ~Client();
+    uint32_t getHost() const;
+    uint16_t getPort() const;
+
+    int loadData();
+    char* getData();
+
+    bool sendData(const char* buffer, const size_t size) const;
+};
+
+#endif // SSERVER_H
---
+++ SupServer/includes.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDES_H
+
+#pragma once
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <iostream>
+#include <WinSock2.h>
+#include <winsock.h>
+#include <cstdint>
+#include <chrono>
+#include <functional>
+#include <thread>
+#include <list>
+#include <string>
+#include "SServer.h"
+
+#endif INCLUDES_H
